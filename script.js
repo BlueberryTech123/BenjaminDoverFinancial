cfa99fddf9aaa0075247c7b71eaeb403efc53e79
JesseIguess <32250628+BlueberryTech123@users.noreply.github.com>
2022-03-18 22:50:25 +0000

    captcha

--- script.js
+++ script.js
@@ -3,16 +3,30 @@ let index = 0;
 const captchaImages = ["captchas/c1.png", "captchas/c2.png", "captchas/c3.png", "captchas/c4.png", "captchas/c5.png"]
 const captchaAnswers = [342*123+32412, 420*69*69, 9, 2, 68824]
 
-function captcha() {
-    let captchaWindow = Document.getElementById("captchaWindow");
-    let captchaImg = Document.getElementById("captchaImg");
+function captcha(valid = true) {
+    let captchaWindow = document.getElementById("captchaWindow");
+    let captchaImg = document.getElementById("captchaImg");
+    let captchaInput = document.getElementById("captchaInput");
     
     index = Math.floor(Math.random() * 4);
     captchaImg.src = captchaImages[index];
     captchaWindow.style.visibility = "visible";
+
+    if (valid) {
+        document.getElementById("errorText").innerHTML = "";
+    }
+    else {
+        document.getElementById("errorText").innerHTML = "Try Again";
+    }
 }
 
 function submitCaptcha() {
-    let captchaInput = Document.getElementById("captchaInput");
-    captchaWindow.style.visibility = "hidden";
+    let captchaInput = document.getElementById("captchaInput");
+    if (captchaInput.value == captchaAnswers[index])
+    {
+        captchaWindow.style.visibility = "hidden";
+        return true;
+    }
+    captcha(false);
+    return false
 }
