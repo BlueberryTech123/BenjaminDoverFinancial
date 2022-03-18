1e9214615946fc577ce677c320a3a6f5b70b7d83
Jesse Li <>
2022-03-18 19:55:24 +0000

    aeiudhwieuf

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const captchaImages = ["captchas/c1.png", "captchas/c2.png", "captchas/c3.png", "captchas/c4.png", "captchas/c5.png"]
+const captchaAnswers = [342*123+32412, 420*69*69, 9, 2, 68824]
 
+function captcha() {
+    //
+}
