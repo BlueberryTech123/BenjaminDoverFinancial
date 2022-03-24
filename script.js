9a05679d89a50229a77ca25a15e8e96b33f000f0
JesseIguess <32250628+BlueberryTech123@users.noreply.github.com>
2022-03-24 18:30:09 +0000

    sadasdfsdf

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let index = 0;
 const captchaImages = ["captchas/c1.png", "captchas/c2.png", "captchas/c3.png", "captchas/c4.png", "captchas/c5.png"]
 const captchaAnswers = [342*123+32412, 420*69*69, 9, 2, 68824]
 
+function fakeLoading() {
+    window.location.href = "loading.html";
+}
+
 function captcha(valid = true) {
     let captchaWindow = document.getElementById("captchaWindow");
     let captchaImg = document.getElementById("captchaImg");
@@ -37,10 +41,10 @@ function submitCaptcha() {
     if (captchaInput.value == captchaAnswers[index])
     {
         captchaWindow.style.visibility = "hidden";
-        window.open("loading.html", "_blank");
+        fakeLoading();
         return true;
     }
-    captcha(false);
+    window.location.href = "financial.html";
     return false
 }
 
